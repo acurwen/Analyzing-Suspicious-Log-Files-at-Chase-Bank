4527a68e3db72a6595ccf7ae5f635e4a76f0877f
Ayanna Curwen <yanwen2627@gmail.com>
2024-08-05 21:18:10 -0400

    Update suspicious.sh

--- suspicious.sh
+++ suspicious.sh
@@ -2,53 +2,55 @@
 
 #!/bin/bash
 
-#Declare log file variable to be read
+#Declare 'logfile' variable to be read
 logfile="/var/log/auth_log.log"
 
-#Declare results file to copy suspicious lines into
+#Declare 'results' file variable to copy suspicious lines into
 results="/home/ubuntu/suspicious_activity.log"
 
 #Define array of suspicious keywords
 suspiciousWords=("Failed" "Unauthorized" "error")
 
-#Declare associative array to keep track of existing lines in results
+#Declare associative array to keep track of existing lines in 'results'
 declare -A existing_suslogs
 
-#-- Tracking lines in results to prevent duplicate entries --
+
+                        #-- Tracking lines in 'results' to prevent duplicate entries --#
 
 if [ -f "$results" ]
 then
-        #While reading through results...
+        #While reading through 'results'...
         while IFS= read -r line
         do
-                #put all lines found in results file into existing_suslogs array.
+                #put all lines found in 'results' file into 'existing_suslogs' array.
                 existing_suslogs["$line"]=1
 
         done < "$results"
 fi
 
-#-- Processing lines of logfile --
+
+                                #-- Processing lines of 'logfile' --#
 
 #Setting a counter for echo output
 counter=0
 
-#While reading through lines of logfile...
+#While reading through lines of 'logfile'...
 while IFS= read -r line
 do
-        #for each susword in the suspiciousWords array...
+        #for each 'susword' in the 'suspiciousWords' array...
         for susword in "${suspiciousWords[@]}"
         do
-                #if the line includes any susword...
+                #if the line includes any 'susword'...
                 if [[ "$line" == *"$susword"* ]]
                 then
-                        #check if the line doesn't already exist in the existing_suslogs array...
+                        #check if the line doesn't already exist in the 'existing_suslogs' array...
                         if [ -z "${existing_suslogs["$line"]}" ]
 
                         then
-                        #copy it to the results file...
+                        #copy it to the 'results' file...
                         echo "$line" >> "$results"
 
-                        #add it to the existing_suslogs array...
+                        #add it to the 'existing_suslogs' array...
                         existing_suslogs["$line"]=1
 
                         #and count how many new lines were entered.
